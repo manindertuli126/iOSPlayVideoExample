97b925c135d07b1b4609547875ef0556258de3ff
moxDroid Labs Inc <pritam.world@gmail.com>
2019-03-19 19:05:27 -0400

    Playing video code added

--- iOSPlayVideoExample/ViewController.swift
+++ iOSPlayVideoExample/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
+    var video = AVPlayer()
+    var videoPlayer = AVPlayerViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func btnPlayAction(_ sender: UIBarButtonItem) {
+        if let path = Bundle.main.path(forResource: "Toronto", ofType: "mp4"){
+            
+            self.video = AVPlayer(url: URL(fileURLWithPath: path))
+            self.videoPlayer = AVPlayerViewController()
+            self.videoPlayer.player = video
+            
+            self.present(self.videoPlayer, animated: true, completion: {
+                self.video.play()
+            })
+        }
+        
     }
 
 
